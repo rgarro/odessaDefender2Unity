b0ad1721cc3582b0a42f614eb1a90bbf7a99652e
rolando <rgarro@gmail.com>
2021-05-17 21:15:13 -0600

    this kind of blue ....

---
+++ Assets/engineController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class engineController : MonoBehaviour
+{
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("e")){
+            this.anim.Play("heliEngineTurn");
+        this.anim.Play("heliEngineTurn1");
+        this.anim.Play("heliEngineTurn3");
+        this.anim.Play("heliEngineTurn4");
+        }
+    }
+}
