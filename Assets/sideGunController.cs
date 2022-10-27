07c7a96cb7ad32322d133f629459d35fb2ad6f98
rgarro <rgarro@gmail.com>
2022-10-27 11:51:55 -0500

    asesinan a embargador de finca en alajuela Boletin Judicial ..

--- Assets/sideGunController.cs
+++ Assets/sideGunController.cs
@@ -8,29 +8,8 @@ using UnityEngine;
  *         ' ~""""""""""""""`##(_))#H\"""""Y########
  *                           ))    \#H\       `"Y###
  *                           "      }#H)
- *  La Cienciologia es Una Secta Gay
- * El SIDA afecta la promiscuidad, pero
- * deshacerce de los gays recatados necesita 
- * de una trampa religiosa. EL SARS y la nanotecnologia
- * provocaran el siguiente genocidio gay. Sigourney Weaver
- * queria vender el planeta a razas comedoras de humanos
- * pero la peste gay hace la carne invendible.
- * Los Colores de la Libertad Gay provocan apetito entre los depredadores, aliens, chupacabras y rigelianos,
- * pero despues de morder un gay piden reembolso en sus bancos provovando la quiebra de Monsanto.
- * El Coronel Kaddafi esta en Macondo, un rigeliano le pregunta , coronel que vamos a comer hoy?
- * El Coronel le contesta iracundo, Mierda !! , no me han pagado los maricas que vaporizaron el mes pasado !!.....
- * ALAbama ALAbama , Macondo esta en un bayou cerca de Mobile, los primos del profeta salvaran el planeta
- * vendiendo los gays, como lo hicieron en Andalusia hace 9 siglos.
- * Garcia Marquez era Gay , los catolicos colombianos le seguian la corriente, no volveria a nacer segun
- * los concilios romanos de la santa inquisicion, Pero se podria vender a un chupacabras y usar los fondos
- * para liberar jerusalem, la guerra en siria no acaba, la venta de carne gay la financia.
- *
- * Los Gays son Bravos, esto le gusta a los depredadores y a los aliens, para lucharlos un poco
- * antes de adobarlos con acido carbonico.
- * Los Aliens evitan ser Gays naciendo de huevos incrustados en lesbianas europeas, coronel kaddafi
- * y sus ministros invirtieron en el Euro y en discotecas por todo el mediterraneo donde los Aliens 
- * se aparean. Cojones Cojones gritaban traficantes de Gays en fuertevetura, en media fiesta rave, 
- * se comen dos lesbianas y tres peluqueros brasileiros.
+ * 
+ * 
  * 
  * 
  *
@@ -56,8 +35,9 @@ public class sideGunController : MonoBehaviour
     }
 
     bool checkIfGunView(){
-        cameras_script = GetComponent<cameras>();
-        return !left_camera_is_hidden;
+        //cameras_script = GetComponent<cameras>();
+        //return !left_camera_is_hidden;
+        return false;
     }
 
     void moveLeft(){
@@ -82,9 +62,21 @@ public class sideGunController : MonoBehaviour
 
     }
 
+    void keyControls(){
+        if (Input.GetKey("a"))
+        {
+           
+        }
+
+        if (Input.GetKey("z"))
+        {
+            
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        this.keyControls();   
     }
 }
