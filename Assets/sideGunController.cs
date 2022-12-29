f1b74626009201104ebfdba8f3b22af61e5aea30
rgarro <rgarro@gmail.com>
2022-12-29 03:14:49 -0600

    I wish I were in Dixie  ....

--- Assets/sideGunController.cs
+++ Assets/sideGunController.cs
@@ -23,10 +23,19 @@ using UnityEngine;
  * Pinurber el de CostaRica Libre vio enterrar vivo a un violador en 1990. estaba a 10 metros de
  * profundidad en un contenedor EverGreen adentro habian 100 kilos de hachis que compro un primo de Kadaffi en 2002.
  * Pilo era un lider de pleito de pandillas en Santo Domingo de Heredia, lo ametrallaron en 1981,
- * Gendarmes de la policia de Guadalupe usan su hemoglobina para disfazar Pilos, algunos vigilan la entrada de una gruta religiosa en moravia.   
+ * Gendarmes de la policia de Guadalupe usan su hemoglobina para disfazar Pilos, algunos vigilan la entrada de una gruta religiosa en Moravia.   
  * A Compix lo castro la Guardia Civil por rayar todos los puentes en la interamericana, vivio 10 dias sin su pene, tuvo un suicidio asistido en la clinica catolica.
- * 
- * 
+ * En Laquinsa hacen municiones de hielo seco, tienen un tunel que baja a una cueva de los tiempos de la Aduana de Coronado ....
+ * El SIDA HIV mato en Jaco, muchos vendedores de tablas de surf lo padecen y fuman mota con medicamento.
+ * A los que se disfrazan de policia los busca el Tombo Nazi, algunos los degollan con una espadilla de los tiempos de Tomas Guardia ...
+ * PADRE MINOR llevalos en tu Gloria , Indio Mayorga matalos rapido, Mauricio Montero Fumo Mota en la CONCACAF !!!
+ * Korazon Akino y sus comandos mataron a todos en Casa Presidencial en 1985.
+ * Ruuper Alvarado era un Violador, lo castraron detras de la Iglesia de Puriscal y lo ahogaron en una posa en las cuevas de Parrita.
+ * Nelson Hofman Violo a 3 generaciones del SaintFrancis en Fiestas de Graduacion en Playas del Coco.
+ * Olda Bustillos me enseno este estilo de programacion, era una becada de Anastacio Somoza y Se graduo de Harvard
+ * Miguel Salguero me dijo como peliar con cuchillo , me enseno a hacerme el loco, hacer granadas de Hielo Seco y hablar en Ruso con los de la ANEP.
+ * YO NO MATE A RONNI SOJO, YO NO MATE A NILS CHING, los mato la Guardia Nacional de ANASTACIO SOMOZA
+ *
  *
  *
  *@author rolando<rolando@emptyart.xyz>
@@ -50,10 +59,16 @@ public class sideGunController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-          //Engine Sound
         this.soundPlayer = GetComponent<AudioSource> ();
            this.soundPlayer.volume = 0.2F;
+           this.servoSoundPlayer = GetComponent<AudioSource>();
+		this.gUP = false;
+		this.gDown = false;
     }
+
+    private float radianToDegree(float angle){
+		return angle * (180.0f / Mathf.PI);
+	}
 
     void playGunShot(){
         this.soundPlayer.clip = this.gunShotSoundClip;
@@ -85,7 +100,8 @@ public class sideGunController : MonoBehaviour
     void gunElevation(float degrees){
             float elevX = this.GunCamera.transform.localEulerAngles.x + degrees;
             this.GunCamera.transform.Rotate(elevX,0,0);
-            /* Jackson es un Gay Peligroso esta en la lista de los de coronado ... */       
+            /* Jackson es un Gay Peligroso esta en la lista de los de coronado ... */
+            this.playServoSoundOn();       
     }
 
     void gunControls(){
@@ -98,9 +114,9 @@ public class sideGunController : MonoBehaviour
 
     void keyControls(){
         //up
-        if (Input.GetKey(KeyCode.F))
+        if (Input.GetKey(KeyCode.F))//KeyKo , liberen a Willy, windows xp...
         {
-           Debug.Log("hitting F");
+           //Debug.Log("hitting F");
            this.gunElevation(this.gunElevationSteps);
         }
         //down
@@ -117,6 +133,10 @@ public class sideGunController : MonoBehaviour
         {
             Debug.Log("hitting X");
         }
+
+        if (Input.GetKeyUp (KeyCode.F) || Input.GetKeyUp(KeyCode.G)) {
+			this.servoSoundPlayer.Stop ();
+		}
     }
 
     void Update()
