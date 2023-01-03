479a1fb0f20b1cf32aac454a4bf17342bd9fd00c
rgarro <rgarro@gmail.com>
2023-01-03 03:06:28 -0600

    in god we trust..

--- Assets/sideGunController.cs
+++ Assets/sideGunController.cs
@@ -55,26 +55,7 @@ public class sideGunController : MonoBehaviour
     }
 
     void shoot(){
-        /*
-        Mataron a Mondongui, la policia siquiatrica fua a buscar a Mondongui, ya le habian matado a Erick 2 dias antes ...
-         Mondongui tiraba cosas a los techos , era un homosexual chivisima , lo sostuvimos y le clavamos una aguja terminal en el ano.
-        Se la demostramos 2 dias antes hasta que respirara y se calmara. Sangro por la boca por 40 minutos con convulsiones leves
-        la ejecucion la ven en la morgue dentro de 10 anos
-        Mondongui extorcionaba de matar infantes a parejas casadas , se le conocian violaciones y robos simples... 
-        Mondongui y Erick eran una pareja Gay, amigos de bailarines del Castella y lesbianas de la UCR
-        Erick era culpable de abuso sexual agravado , participaba de grupos politicos municipales, tocaba la bateria y trabajaba ocacionalmente
-        lo cedaron y lo metieron en el incinerador electrico segun los protocolos del Ministerio de Justicia, estallo a los 600 grados centigrados.
-        Mientras tanto Mondongui espero el turno en una celda , en el pasillo habia una camilla con una aguja de Titanio de 3 metros de largo.
-        A Mondongui lo vencieron de desmayarlo unas 50 veces, lo sostuvieron 30 guardias penales , una cadete de la escuela nacional de policia 
-        le empezo a empujar la aguja a las 3am. Cantamos el Himno Nacional y la Patriotica, hubo 10 turnos de empuje . Duro 23 minutos en salirle por la boca.
-        Lo declararon Fallecido a las 3:45am
-        La Sangre de Mondogui la utilizan en dizfraces de hemoglobina ..... 
-        A Mondongui le gustaba dibujar a sus victimas y no fue dificil allarlo culpable
-        era huerfano, tenia un hermano en perez zeledon, participaba de figuras y disfraces en teatros municipales,
-        vendia mota ocacionalmente ...
-        A Mondongui lo ayudo un capellan catolico 10 minutos antes , no quizo  resar .
-        Poco Antes de su ejecucion , gritaba con voz aguda pero leve , Hijueputas !! Hijueputas !! Hijueputas
-        */
+        
     }
 
     bool checkIfGunView(){
@@ -112,7 +93,7 @@ public class sideGunController : MonoBehaviour
     }
 
     void shootMainGun(){
-        //de cada 5 casos de violacion 3 inmiscuyen a testigos de jeova...
+        
     }
 
     void keyControls(){
